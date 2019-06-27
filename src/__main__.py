105d48a859a2910f46647b2e3542f23a1bda3b6b
NicolasBi <nicolasbizzozzero@gmail.com>
2019-06-27 19:31:35 +0200

    zhbudbzhdadba

--- src/__main__.py
+++ src/__main__.py
@@ -5,31 +5,33 @@ from .factory import build_all
 
 def main():
     configs = {
-        "energies": {"energies": [
-            {
-                "name": "Carbon",
-                "cost": 0.2,
-                "amount": 100,
-                "pollution_factor": 1,
-            },
-            {
-                "name": "Nuclear",
-                "cost": 0.5,
-                "amount": 40,
-                "pollution_factor": 0,
-            },
-            {
-                "name": "Renewable",
-                "cost": 1,
-                "amount": 10,
-                "pollution_factor": 0,
-            }
-        ]},
+        "energies": {
+            "energies": [
+                    {
+                        "name": "Carbon",
+                        "cost": 0.2,
+                        "amount": 100,
+                        "pollution_factor": 1,
+                    },
+                    {
+                        "name": "Nuclear",
+                        "cost": 0.5,
+                        "amount": 40,
+                        "pollution_factor": 0,
+                    },
+                    {
+                        "name": "Renewable",
+                        "cost": 1,
+                        "amount": 10,
+                        "pollution_factor": 0,
+                    }
+            ]
+        },
         "people": {},
         "lights": 5_000,
         "heaters": 3_000
     }
-    
+
     data = build_all(configs)
     local_vars = locals()
     local_vars = {**local_vars, **configs}
--- src/factory.py
+++ src/factory.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-from energy import Energies, identity_sample
+from .energy import Energies, identity_sample
+
 
 def buil_all(configs):
     energies = build_energies(configs["energies"])
@@ -25,13 +26,12 @@ def build_energies(config):
     sampler_amount_factors = identity_sample
 
     return Energies(
-        initial_costs = costs,
-        initial_amounts = amounts,
-        pollution_factors = pollution_factors,
-        sampler_cost_factors = sampler_cost_factors,
-        sampler_amount_factors = sampler_amount_factors
+        initial_costs=costs,
+        initial_amounts=amounts,
+        pollution_factors=pollution_factors,
+        sampler_cost_factors=sampler_cost_factors,
+        sampler_amount_factors=sampler_amount_factors
     )
-    
 
 
 def build_people(config):
